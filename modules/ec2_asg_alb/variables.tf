2743edf9ef8375efcfa1b42c5cfd749c47abf722
shubham1997 <shubhamba97@gmail.com>
2025-10-18 11:29:43 +0530

    Add variables for EC2 ASG and ALB configuration

---
+++ modules/ec2_asg_alb/variables.tf
@@ -0,0 +1,14 @@
+variable "project_name" { type = string }
+variable "vpc_id" { type = string }
+variable "public_subnet_ids" { type = list(string) }
+variable "private_subnet_ids" { type = list(string) }
+variable "web_sg_id" { type = string }
+variable "alb_sg_id" { type = string }
+variable "db_endpoint" { type = string }
+variable "instance_type" { type = string }
+variable "key_name" { type = string }
+variable "desired_capacity" { type = number }
+variable "asg_min_size" { type = number }
+variable "asg_max_size" { type = number }
+variable "env" { type = string }
+variable "alb_certificate_arn" { type = string }
